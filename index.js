f396f15d5f333b4586c5fc6d63508eb4fd48cff4
OT <omartemsah99@gmail.com>
2025-05-22 13:45:47 +0300

    Creating a logger using winston

--- index.js
+++ index.js
@@ -16,19 +16,66 @@ const express = require("express");
 const app = express();
 
 // Configure winston logger
-winston.add(
-  new winston.transports.Console({
-    format: winston.format.simple(),
-  })
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new winston.transports.File({
+      filename: "logfile.log",
+    }),
+    new winston.transports.MongoDB({
+      db: "mongodb://localhost/vidly",
+      level: "error",
+      collection: "logs",
+    }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new winston.transports.File({
+      filename: "exceptions.log",
+    }),
+    new winston.transports.MongoDB({
+      db: "mongodb://localhost/vidly",
+      collection: "logs",
+    }),
+  ],
+  rejectionHandlers: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new winston.transports.File({
+      filename: "rejections.log",
+    }),
+    new winston.transports.MongoDB({
+      db: "mongodb://localhost/vidly",
+      collection: "logs",
+    }),
+  ],
+});
+
+// Exit on uncaught exceptions and rejections
+winston.exceptions.handle(
+  new winston.transports.Console({ format: winston.format.simple() }),
+  new winston.transports.File({ filename: "exceptions.log" })
 );
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vidly",
-    level: 'error',
-    collection: "logs",
-  })
+
+winston.rejections.handle(
+  new winston.transports.Console({ format: winston.format.simple() }),
+  new winston.transports.File({ filename: "rejections.log" })
 );
+
+process.on("exit", () => {
+  winston.info("Process exiting...");
+});
+
+// const p = Promise.reject(new Error("Somthing failed miserably"));
+// p.then(() => console.log("Done"));
+// throw new Error("Something Failed during start up");
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
