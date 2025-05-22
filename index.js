1915b595cb4d2471bcb58ca1fd1e559d2db45969
OT <omartemsah99@gmail.com>
2025-05-22 15:56:25 +0300

    extracting config

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-require("express-async-errors");
-const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
@@ -8,13 +6,9 @@ const app = express();
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/config")();
 
 // throw new Error("Test Error.");
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
