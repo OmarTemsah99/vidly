4aa160431184545adccbdec756b56f5924f5f66b
OT <omartemsah99@gmail.com>
2025-05-22 16:03:21 +0300

    fixing the logger

--- startup/logging.js
+++ startup/logging.js
@@ -3,58 +3,47 @@ const winston = require("winston");
 require("winston-mongodb");
 
 module.exports = function () {
-  const logger = winston.createLogger({
+  winston.configure({
     level: "info",
     format: winston.format.simple(),
     transports: [
       new winston.transports.Console({
-        format: winston.format.simple(),
+        format: winston.format.simple()
       }),
       new winston.transports.File({
-        filename: "logfile.log",
+        filename: "logfile.log"
       }),
       new winston.transports.MongoDB({
         db: "mongodb://localhost/vidly",
         level: "error",
-        collection: "logs",
-      }),
+        collection: "logs"
+      })
     ],
     exceptionHandlers: [
       new winston.transports.Console({
-        format: winston.format.simple(),
+        format: winston.format.simple()
       }),
       new winston.transports.File({
-        filename: "exceptions.log",
+        filename: "exceptions.log"
       }),
       new winston.transports.MongoDB({
         db: "mongodb://localhost/vidly",
-        collection: "logs",
-      }),
+        collection: "logs"
+      })
     ],
     rejectionHandlers: [
       new winston.transports.Console({
-        format: winston.format.simple(),
+        format: winston.format.simple()
       }),
       new winston.transports.File({
-        filename: "rejections.log",
+        filename: "rejections.log"
       }),
       new winston.transports.MongoDB({
         db: "mongodb://localhost/vidly",
-        collection: "logs",
-      }),
-    ],
+        collection: "logs"
+      })
+    ]
   });
-
-  // Exit on uncaught exceptions and rejections
-  winston.exceptions.handle(
-    new winston.transports.Console({ format: winston.format.simple() }),
-    new winston.transports.File({ filename: "exceptions.log" })
-  );
-
-  winston.rejections.handle(
-    new winston.transports.Console({ format: winston.format.simple() }),
-    new winston.transports.File({ filename: "rejections.log" })
-  );
 
   process.on("exit", () => {
     winston.info("Process exiting...");
